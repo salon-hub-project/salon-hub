8d2381c11fcf470c3b55dd72d91e249c93c9af0a
Nidhiyuvasoft <nidhiyuvasoft455@gmail.com>
2026-02-02 10:54:11 +0530

    fixed baseurl

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,7 +1,5 @@
 export const config = {
-
-  // API_BASE_URL: "https://orlando-linelike-marylynn.ngrok-free.dev/api/v1"
-  API_BASE_URL:"https://salonproject001.vercel.app/api/v1",
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
 } as const;
 
 export default config;
