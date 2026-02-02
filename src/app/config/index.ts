0e714a4c912c0cabe618e3cdcd7f1bfd0f92b161
Nidhiyuvasoft <nidhiyuvasoft455@gmail.com>
2026-02-02 11:50:06 +0530

    minor changes

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,6 +1,5 @@
 export const config = {
-  // API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
-  API_BASE_URL: "https://salonproject001.vercel.app/api/v1",
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL!,
 } as const;
-console.log(config.API_BASE_URL, "urlllllll");
+
 export default config;
