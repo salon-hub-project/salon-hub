f0b2af8b4440ba6376b963bb643493630a8e6346
Nidhiyuvasoft <nidhiyuvasoft455@gmail.com>
2026-02-02 11:20:32 +0530

    fixed url

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,31 +1,6 @@
 export const config = {
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+  // API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+  API_BASE_URL: "https://salonproject001.vercel.app/api/v1",
 } as const;
 
 export default config;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
